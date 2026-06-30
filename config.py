3c44aa2a080a9e3817b0b2cffbd0239354c1c776
yashtripathi20005-creator <yashtripathi20005@gmail.com>
2026-06-30 16:03:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///job_applications.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
